3f596959833ad266af85c5231c3689532ccc8a6a
Ivanuschka <ivan.arzyutov2000@gmail.com>
2023-09-06 17:05:08 +0300

    PDF

--- IDZ1.py
+++ IDZ1.py
@@ -1,12 +1,9 @@
 n = int(input("Введите натуральное число: "))
 
-# Определяем последнюю цифру числа
 last_digit = n % 10
 
-# Определяем предпоследнюю цифру числа (если она есть)
 second_last_digit = n % 100 // 10
 
-# Проверяем условия для склонения слова "год"
 if second_last_digit != 1 and last_digit == 1:
     word = "год"
 elif 2 <= last_digit <= 4 and (second_last_digit != 1 or second_last_digit is None):
@@ -14,5 +11,4 @@ elif 2 <= last_digit <= 4 and (second_last_digit != 1 or second_last_digit is No
 else:
     word = "лет"
 
-# Выводим фразу на экран
 print(f"Мне {n} {word}.")
